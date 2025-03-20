f0bc0f4fa73fd6ee7d4c105b04d374a21d13b88d
Massimiliano Tomassoli <kiuhnm03@yahoo.it>
2025-03-20 05:35:45 +0100

    Rename some enums and traits for clarity

--- src/overloading.rs
+++ src/overloading.rs
@@ -30,19 +30,19 @@ pub struct AsTrait1<'a, T: Trait1>(pub &'a T);
 #[derive(Clone, Copy)]
 pub struct AsTrait2<'a, T: Trait2>(pub &'a T);
 
-enum AllTypes {
-    Type1,
-    Type2,
+enum AllTraits {
+    Trait1,
+    Trait2,
 }
 
-trait WithTrait1Or2 {
-    const TYPE: AllTypes;
+trait AsTrait1Or2 {
+    const TRAIT: AllTraits;
     fn t1(&self) -> &impl Trait1;
     fn t2(&self) -> &impl Trait2;
 }
 
-impl<T: Trait1> WithTrait1Or2 for AsTrait1<'_, T> {
-    const TYPE: AllTypes = AllTypes::Type1;
+impl<T: Trait1> AsTrait1Or2 for AsTrait1<'_, T> {
+    const TRAIT: AllTraits = AllTraits::Trait1;
 
     fn t1(&self) -> &impl Trait1 {
         self.0
@@ -53,8 +53,8 @@ impl<T: Trait1> WithTrait1Or2 for AsTrait1<'_, T> {
     }
 }
 
-impl<T: Trait2> WithTrait1Or2 for AsTrait2<'_, T> {
-    const TYPE: AllTypes = AllTypes::Type2;
+impl<T: Trait2> AsTrait1Or2 for AsTrait2<'_, T> {
+    const TRAIT: AllTraits = AllTraits::Trait2;
 
     fn t1(&self) -> &impl Trait1 {
         &NO_IMPL_TRAIT1
@@ -72,34 +72,34 @@ pub enum FResult {
 }
 
 #[allow(private_bounds)]
-pub fn f<T1: WithTrait1Or2, T2: WithTrait1Or2>(x: T1, y: T2) -> FResult {
+pub fn f<T1: AsTrait1Or2, T2: AsTrait1Or2>(x: T1, y: T2) -> FResult {
     // NOTE: It's important to ALWAYS check for exhaustiveness.
-    match (T1::TYPE, T2::TYPE) {
-        (AllTypes::Type2, _) => FResult::Str(x.t2().method2()),
-        (_, AllTypes::Type2) => FResult::Str(y.t2().method2()),
-        (AllTypes::Type1, AllTypes::Type1) => FResult::IntInt(
+    match (T1::TRAIT, T2::TRAIT) {
+        (AllTraits::Trait2, _) => FResult::Str(x.t2().method2()),
+        (_, AllTraits::Trait2) => FResult::Str(y.t2().method2()),
+        (AllTraits::Trait1, AllTraits::Trait1) => FResult::IntInt(
             x.t1().method1(), y.t1().method1()),
     }
 }
 
-enum XorTypes {
-    Types1And2,
-    Types2And1,
+enum XorTraits {
+    Traits1And2,
+    Traits2And1,
 }
 
-trait PairWithTraits1Xor2 {
-    const TYPES: XorTypes;
+trait PairAsTraits1Xor2 {
+    const TRAITS: XorTraits;
 
     fn t12(&self) -> (&impl Trait1, &impl Trait2);
     fn t21(&self) -> (&impl Trait2, &impl Trait1);
 }
 
-impl<T1, T2> PairWithTraits1Xor2 for (AsTrait1<'_, T1>, AsTrait2<'_, T2>)
+impl<T1, T2> PairAsTraits1Xor2 for (AsTrait1<'_, T1>, AsTrait2<'_, T2>)
 where
     T1: Trait1,
     T2: Trait2,
 {
-    const TYPES: XorTypes = XorTypes::Types1And2;
+    const TRAITS: XorTraits = XorTraits::Traits1And2;
 
     fn t12(&self) -> (&impl Trait1, &impl Trait2) {
         (self.0.0, self.1.0)
@@ -110,12 +110,12 @@ where
     }
 }
 
-impl<T1, T2> PairWithTraits1Xor2 for (AsTrait2<'_, T1>, AsTrait1<'_, T2>)
+impl<T1, T2> PairAsTraits1Xor2 for (AsTrait2<'_, T1>, AsTrait1<'_, T2>)
 where
     T1: Trait2,
     T2: Trait1,
 {
-    const TYPES: XorTypes = XorTypes::Types2And1;
+    const TRAITS: XorTraits = XorTraits::Traits2And1;
 
     fn t12(&self) -> (&impl Trait1, &impl Trait2) {
         (&NO_IMPL_TRAIT1, &NO_IMPL_TRAIT2)
@@ -133,13 +133,13 @@ pub enum FXorResult {
 }
 
 #[allow(private_bounds)]
-pub fn f_xor<P: PairWithTraits1Xor2>(x_y: P) -> FXorResult {
-    match P::TYPES {
-        XorTypes::Types1And2 => {
+pub fn f_xor<P: PairAsTraits1Xor2>(x_y: P) -> FXorResult {
+    match P::TRAITS {
+        XorTraits::Traits1And2 => {
             let (x, y) = x_y.t12();
             FXorResult::IntStr(x.method1(), y.method2())
         }
-        XorTypes::Types2And1 => {
+        XorTraits::Traits2And1 => {
             let (x, y) = x_y.t21();
             FXorResult::StrInt(x.method2(), y.method1())
         }
